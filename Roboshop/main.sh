3fc19a88caebcfd3ff42d52a17d94edae8b1c5ef
crishireddy <chimullarishireddy02@gmail.com>
2022-01-20 23:13:17 -0500

    created cart

---
+++ Roboshop/main.sh
@@ -0,0 +1,6 @@
+#!/ban/bash
+
+# this will take the main argument form the user and that script will be excuted, i.e. all the excution will be written
+#main file and rest file is used to store commands, functions
+
+sh components/$1.sh

190b725826c2847cdef593db2995c30c72815a56
crishireddy <chimullarishireddy02@gmail.com>
2022-01-27 22:58:45 -0500

    edited Frontend.sh

--- Roboshop/components/Frontend.sh
+++ Roboshop/components/Frontend.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -e
 
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
@@ -13,7 +12,6 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
 systemctl restart nginx
 
 #this is the main script, the only Problem with this script is if any one command fails insted of stopping it

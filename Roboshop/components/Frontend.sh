6e1250bb49110c9acb7b7c05be26a9a4bcf19dee
crishireddy <chimullarishireddy02@gmail.com>
2022-02-01 21:31:55 -0500

    Edited  common.sh

--- Roboshop/components/Frontend.sh
+++ Roboshop/components/Frontend.sh
@@ -8,7 +8,7 @@ source components/common.sh
 
 Print "Installing Nginx" "yum install nginx -y"
 yum install nginx -y
-stat $?
+Stat $?
 #stat $? stat functon we will make in common and $? is a exit status of our yum command
 #
 Print "Starting Nginx Service" "systemctl enable nginx"

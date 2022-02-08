676bb0c26ebdd9a7974322f64119086f443c3ec6
crishireddy <chimullarishireddy02@gmail.com>
2022-02-07 21:56:57 -0500

    edited create-instance.sh file

--- Roboshop/create-instance.sh
+++ Roboshop/create-instance.sh
@@ -6,5 +6,5 @@ if [ -z "${component}" ]; then
   echo "Need a Input of component Name"
   exit 1
 fi
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-0aa25e272bc927222  --tag-specifications "ResourceType=instance,Tags=[{key=Name,
-value=${component}}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-0aa25e272bc927222 --tag-specifications "ResourceType=instance,Tags=[{key=Name,
+ value=${component}}]"

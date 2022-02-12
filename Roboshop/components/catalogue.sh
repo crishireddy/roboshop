2e92061d4f1fa4d8b12d18f63f0983fc03f6d630
crishireddy <chimullarishireddy02@gmail.com>
2022-02-12 17:58:08 -0500

    created MongoDB.sh file

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo welcome to catalogue
+source components/common.sh
+
+Print "Installing nodejs" " install nodejs make gcc-c++ -Y "
+yum install nodejs make gcc-c++ -y
+Stat $?
+
+Print "Adding Roboshop Project user" "useradd roboshop"
+useradd roboshop
+Stat $?
+
+Print "Download Catalogue component code" " curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Stat $?
+
+Print ""Extract catalogue component code" "rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
+rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
+Stat $?
+exit
+
+$ npm install
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue

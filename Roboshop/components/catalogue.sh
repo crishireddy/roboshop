d3a58ca5f06ee78dd0a03d28ad7bcdec378035f7
crishireddy <chimullarishireddy02@gmail.com>
2022-02-12 21:08:25 -0500

    created MongoDB.sh file

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -6,7 +6,7 @@ Print "Installing nodejs" " install nodejs make gcc-c++ -Y "
 yum install nodejs make gcc-c++ -y
 Stat $?
 
-Print "Adding Roboshop Project user" "useradd roboshop"
+Print "Adding roboshop Project user" "useradd roboshop"
 id roboshop || useradd roboshop
 Stat $?
 
@@ -14,7 +14,7 @@ Print "Download Catalogue component code" " curl -s -L -o /tmp/catalogue.zip "ht
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 Stat $?
 
-Print ""Extract catalogue component code" "rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
+Print "Extract catalogue component code rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
 rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
 Stat $?
 exit
